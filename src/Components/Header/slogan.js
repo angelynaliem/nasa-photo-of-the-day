801a5d3ddf71a133e80cae4c41ee84c77a996d82
angelynaliem <angelynaliem@gmail.com>
2020-07-26 01:40:14 -0400

    Added slogan.js

---
+++ src/Components/Header/slogan.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import styled from 'styled-components';
+
+const SloganStyle = styled.h1 `
+    margin: 0 20px;
+`;
+
+const Slogan = ()=>{
+    return (
+        <SloganStyle>
+            Be Salt, Be Light
+    </SloganStyle>
+    )
+
+}
+
+export default Slogan
